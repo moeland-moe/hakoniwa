4420607e2b35007e25b2ae97bad60ef16dffcc5b
moeland-moe <info@moeland.moe>
2019-03-01 06:32:44 +0900

    インデント修正。

--- app/views/map/island-infolist.php
+++ app/views/map/island-infolist.php
@@ -1,53 +1,53 @@
 <tr>
-		<th class="TitleCell head"><?=$init->nameRank ?></th>
-		<th class="TitleCell head"><?=$init->nameSuffix ?></th>
-		<th class="TitleCell head">得点</th>
-		<th class="TitleCell head"><?=$init->namePopulation ?></th>
-		<th class="TitleCell head"><?=$init->nameArea ?></th>
-		<th class="TitleCell head"><?=$init->nameWeather ?></th>
-		<th class="TitleCell head"><?=$init->nameFunds ?> <?=$lots ?></th>
-		<th class="TitleCell head"><?=$init->nameFood ?></th>
-		<th class="TitleCell head"><?=$init->nameUnemploymentRate ?></th>
-	</tr>
-	<tr>
-		<th class="NumberCell number" rowspan=5><?=$j ?></th>
-		<td class="NameCell" rowspan=4>
-			<h3><a href="$this_file?Sight=$id"><?=$name ?></a><?=$keep ?><?=$start ?></h3>
-            <div><?=$monster ?> <?=$soccer ?></div>
-			<div><?=$prize ?> <?=$viking ?></div>
-            <div><?=$zins ?></div>
-		</td>
-		<td class="InfoCell"><?=$point ?></td>
-		<td class="InfoCell"><?=$pop ?></td>
-		<td class="InfoCell"><?=$area ?></td>
-		<td class="TenkiCell"><?=$sora ?></td>
-		<td class="InfoCell"><?=$money ?></td>
-		<td class="InfoCell"><?=$food ?></td>
-		<td class="InfoCell"><?=$unemployed ?></td>
-	</tr>
-	<tr>
-		<th class="TitleCell head"><?=$init->nameFarmSize ?></th>
-		<th class="TitleCell head"><?=$init->nameFactoryScale ?></th>
-		<th class="TitleCell head"><?=$init->nameCommercialScale ?></th>
-		<th class="TitleCell head"><?=$init->nameMineScale ?></th>
-		<th class="TitleCell head"><?=$init->namePowerPlantScale ?></th>
-		<th class="TitleCell head"><?=$init->namePowerSupplyRate ?></th>
-		<th class="TitleCell head"><?=$init->nameSatellite ?></th>
-	</tr>
-	<tr>
-		<td class="InfoCell"><?=$farm ?></td>
-		<td class="InfoCell"><?=$factory ?></td>
-		<td class="InfoCell"><?=$commerce ?></td>
-		<td class="InfoCell"><?=$mountain ?></td>
-		<td class="InfoCell"><?=$hatuden ?></td>
-		<td class="InfoCell"><?=$ene ?></td>
-		<td class="ItemCell"><?=$eiseis ?></td>
-	</tr>
-	<tr>
-		<th class="TitleCell head">取得アイテム</th>
-		<td class="ItemCell" colspan=6><?=$items ?></td>
-	</tr>
-	<tr>
-        <td class="NameCell"><small>前ターン比： <?=$poin ?> / <?=$peop ?> / <?=$okane ?> / <?=$gohan ?></small></td>
-		<td class="CommentCell" colspan=7><span class="head"><?=$owner ?>：</span> <?=$comment ?></td>
-	</tr>
+    <th class="TitleCell head"><?=$init->nameRank ?></th>
+    <th class="TitleCell head"><?=$init->nameSuffix ?></th>
+    <th class="TitleCell head">得点</th>
+    <th class="TitleCell head"><?=$init->namePopulation ?></th>
+    <th class="TitleCell head"><?=$init->nameArea ?></th>
+    <th class="TitleCell head"><?=$init->nameWeather ?></th>
+    <th class="TitleCell head"><?=$init->nameFunds ?> <?=$lots ?></th>
+    <th class="TitleCell head"><?=$init->nameFood ?></th>
+    <th class="TitleCell head"><?=$init->nameUnemploymentRate ?></th>
+</tr>
+<tr>
+    <th class="NumberCell number" rowspan=5><?=$j ?></th>
+    <td class="NameCell" rowspan=4>
+        <h3><a href="$this_file?Sight=$id"><?=$name ?></a><?=$keep ?><?=$start ?></h3>
+        <div><?=$monster ?> <?=$soccer ?></div>
+        <div><?=$prize ?> <?=$viking ?></div>
+        <div><?=$zins ?></div>
+    </td>
+    <td class="InfoCell"><?=$point ?></td>
+    <td class="InfoCell"><?=$pop ?></td>
+    <td class="InfoCell"><?=$area ?></td>
+    <td class="TenkiCell"><?=$sora ?></td>
+    <td class="InfoCell"><?=$money ?></td>
+    <td class="InfoCell"><?=$food ?></td>
+    <td class="InfoCell"><?=$unemployed ?></td>
+</tr>
+<tr>
+    <th class="TitleCell head"><?=$init->nameFarmSize ?></th>
+    <th class="TitleCell head"><?=$init->nameFactoryScale ?></th>
+    <th class="TitleCell head"><?=$init->nameCommercialScale ?></th>
+    <th class="TitleCell head"><?=$init->nameMineScale ?></th>
+    <th class="TitleCell head"><?=$init->namePowerPlantScale ?></th>
+    <th class="TitleCell head"><?=$init->namePowerSupplyRate ?></th>
+    <th class="TitleCell head"><?=$init->nameSatellite ?></th>
+</tr>
+<tr>
+    <td class="InfoCell"><?=$farm ?></td>
+    <td class="InfoCell"><?=$factory ?></td>
+    <td class="InfoCell"><?=$commerce ?></td>
+    <td class="InfoCell"><?=$mountain ?></td>
+    <td class="InfoCell"><?=$hatuden ?></td>
+    <td class="InfoCell"><?=$ene ?></td>
+    <td class="ItemCell"><?=$eiseis ?></td>
+</tr>
+<tr>
+    <th class="TitleCell head">取得アイテム</th>
+    <td class="ItemCell" colspan=6><?=$items ?></td>
+</tr>
+<tr>
+    <td class="NameCell"><small>前ターン比： <?=$poin ?> / <?=$peop ?> / <?=$okane ?> / <?=$gohan ?></small></td>
+    <td class="CommentCell" colspan=7><span class="head"><?=$owner ?>：</span> <?=$comment ?></td>
+</tr>
