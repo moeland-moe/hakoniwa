4bf1dc95a4e4e98b7d99c0ab2d0f5cd8ca060484
moeland-moe <info@moeland.moe>
2019-03-04 21:12:02 +0900

    テンプレート外だし。

--- app/presenter/hako-html.php
+++ app/presenter/hako-html.php
@@ -184,50 +184,7 @@ class HTML
             $ene = round($island['hatuden'] / $enesyouhi * 100);
             $ene = ($ene < 100) ? '<span style="color:#c7243a;">'.$ene.'%</span>' : $ene.'%';
         }
-        echo <<<END
-    <tr>
-        <th class="NumberCell number" rowspan=5>$j</th>
-        <td class="NameCell" rowspan=4>
-            <h3><a href="$this_file?Sight=$id">$name</a>$keep$start</h3>
-            <div>$monster $soccer</div>
-            <div>$prize $viking</div>
-            <div>$zins</div>
-        </td>
-        <td class="InfoCell">$point</td>
-        <td class="InfoCell">$pop</td>
-        <td class="InfoCell">$area</td>
-        <td class="TenkiCell">$sora</td>
-        <td class="InfoCell">$money</td>
-        <td class="InfoCell">$food</td>
-        <td class="InfoCell">$unemployed</td>
-    </tr>
-    <tr>
-        <th class="TitleCell head">$init->nameFarmSize</th>
-        <th class="TitleCell head">$init->nameFactoryScale</th>
-        <th class="TitleCell head">$init->nameCommercialScale</th>
-        <th class="TitleCell head">$init->nameMineScale</th>
-        <th class="TitleCell head">$init->namePowerPlantScale</th>
-        <th class="TitleCell head">$init->namePowerSupplyRate</th>
-        <th class="TitleCell head">$init->nameSatellite</th>
-    </tr>
-    <tr>
-        <td class="InfoCell">$farm</td>
-        <td class="InfoCell">$factory</td>
-        <td class="InfoCell">$commerce</td>
-        <td class="InfoCell">$mountain</td>
-        <td class="InfoCell">$hatuden</td>
-        <td class="InfoCell">$ene</td>
-        <td class="ItemCell">$eiseis</td>
-    </tr>
-    <tr>
-        <th class="TitleCell head">取得アイテム</th>
-        <td class="ItemCell" colspan=6>$items</td>
-    </tr>
-    <tr>
-        <td class="NameCell"><small>前ターン比： $poin / $peop / $okane / $gohan</small></td>
-        <td class="CommentCell" colspan=7><span class="head">{$owner}：</span> $comment</td>
-    </tr>
-END;
+        require_once VIEWS."/map/island-infotable.php";
     }
 
     private function calculations(string $cat, array $island) :float
